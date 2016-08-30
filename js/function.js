bd699be924968a3f6f7871c983c82381f40858ed
Constantine Pustovoy <pustovoylug@gmail.com>
2016-08-30 18:52:25 +0300

    final

--- js/function.js
+++ js/function.js
@@ -1,31 +1,51 @@
 // alert('hello world');
 // console.log(alert)
 
+// var arr = [];
+// for (var i = 0; i < 5; i++) {
+// 	arr[i] = prompt('Введите имя пользователя', '')
+// 	console.log(typeof arr[i], arr[i]);
+// }
+// console.log(arr);
+
+// var name = prompt('Введите имя пользователя для получения доступа', '');
+// console.log(typeof name, name);
 var arr = [];
-arr.length = 5;
-for (var i = 0; i < arr.length; i++) {
+var name;
+
+function init() {
+	for (var i = 0; i < 5; i++) {
 	arr[i] = prompt('Введите имя пользователя', '')
 	console.log(typeof arr[i], arr[i]);
 }
-console.log(arr);
+	console.log(arr);
 
-var name = prompt('Введите имя пользователя для получения доступа', '');
-console.log(typeof name, name);
+	name = prompt('Введите имя пользователя для получения доступа', '');
+	console.log(typeof name, name);
+}
 
 function search(arr, name) {
-	for (var i = 0, l = arr.length; i < l; i++) {
+	for (var i = 0; i < arr.length; i++) {
 	if (arr[i] === name) {
-	alert(name + ', Вы успешно вошли в систему');
-	break;
-	} else if (arr[i] != name){
-		continue;
-	} else {
-	alert('Попытка несанкционированного доступа запротоколирована');
+	console.log(arr.indexOf[i]);
+	return true;
+	} 
 }
-}
+	return false;
 }
 
-search(arr, name);
+function login() {
+	if (search(arr, name) === true) {
+		alert(name + ', Вы успешно вошли в систему');
+	} else {
+		alert('В доступе отказано');
+	};
+};
+
+init();
+login ();
+
+// search(arr, name);
 
 
 
